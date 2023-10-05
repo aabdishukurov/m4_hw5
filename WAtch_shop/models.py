fcd71ed65f10f54ee8163a9a1a0fcec4c2f9f976
aabdishukurov <shock85@list.com>
2023-10-05 19:08:53 +0600

    first commit

--- WAtch_shop/models.py
+++ WAtch_shop/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+ADMIN = 1
+VIPClient = 2
+CLIENT = 3
+
+USER_TYPE = (
+    (ADMIN, 'Администратор'),
+    (VIPClient, 'VIP Клиент'),
+    (CLIENT, 'Клиент')
+)
+
+MALE = 1
+FEMALE = 2
+
+GENDER_TYPE = (
+    (MALE, 'М'),
+    (FEMALE, 'Ж')
+)
+
+class CustomUser(User):
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    user_type = models.IntegerField(choices=USER_TYPE,
+                                    verbose_name="Выберите тип пользователя")
+    phone_number = models.CharField('ваш сотовый:', max_length=13)
+    age = models.PositiveIntegerField('Укажите возраст?', default=15)
+    gender = models.IntegerField(choices=GENDER_TYPE, verbose_name='Ваш пол')
 
 
 class Shop(models.Model):
@@ -25,22 +55,3 @@ class Shop(models.Model):
         verbose_name = 'Часы'
         verbose_name_plural = 'Часы'
 
-class Comment(models.Model):
-     STARS = (
-         ('Оценка: 1', 'Оценка: 1'),
-         ('Оценка: 2', 'Оценка: 2'),
-         ('Оценка: 3', 'Оценка: 3'),
-         ('Оценка: 4', 'Оценка: 4'),
-         ('Оценка: 5', 'Оценка: 5')
-     )
-     review_object = models.CharField('Укажите название часов:', max_length=100)
-     review_created_at = models.DateTimeField(auto_now_add=True)
-     review_text = models.TextField('Отзыв:', blank=True, null=True)
-     review_start = models.CharField('Укажите свою оценку:', max_length=100, choices=STARS)
-
-     def __str__(self):
-         return self.review_start
-
-     class Meta:
-         verbose_name = 'Отзывы'
-         verbose_name_plural = 'Отзывы'
--- WAtch_shop/views.py
+++ WAtch_shop/views.py
@@ -2,6 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
 from django.views import generic
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
+from django.views.generic import CreateView, ListView
+from . import forms
+
+class RegistrationView(CreateView):
+    form_class = forms.RegistraionNewForm
+    #form_class = UserCreationForm
+    success_url = '/users/'
+    template_name = 'registration/registration.html'
+
+class AuthLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        return reverse('users:user_list')
+
+class UserListView(ListView):
+    queryset = User.objects.all()
+    template_name = 'registration/user_list.html'
+
+    def get_queryset(self):
+        return User.objects.all()
 
 
 
@@ -51,9 +78,6 @@ class UpdateWatchShopView(generic.UpdateView):
     def form_valid(self, form):
         return super(UpdateWatchShopView, self).form_valid(form=form)
 
-def Comment_View(request):
-    comment = models.Comment.objects.all()
-    return render(request, 'watch_detail.html', {'comment_key': comment})
 
 class Search(generic.ListView):
     template_name = 'watches/shop.html'
